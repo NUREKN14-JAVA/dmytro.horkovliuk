eda22218eff11c282736e4d8486e183bdf88863e
Dmitry Gorkovluk <Dmitry Gorkovluk>
2016-12-26 06:10:06 +0200

    webapp complete

--- src/test/java/com/nixsolutions/web/BrowseServletTest.java
+++ src/test/java/com/nixsolutions/web/BrowseServletTest.java
@@ -9,7 +9,17 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-public class BrowseServletTest  extends MockServletTestCase {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.nixsolutions.usermanagement.User;
+
+public class BrowseServletTest extends MockServletTestCase {
 
     @Before
     protected void setUp() throws Exception {
@@ -27,6 +37,18 @@ public class BrowseServletTest  extends MockServletTestCase {
         Collection collection = (Collection) getWebMockObjectFactory().getMockSession().getAttribute("users");
         assertNotNull("Could not find list of users in session", collection);
         assertSame(list, collection);
+    }
+
+    @Test
+    public void testEdit() {
+        User user = new User(new Long(1000), "John", "Doe", new Date());
+        getMockUserDao().expectAndReturn("find", new Long(1000), user);
+        addRequestParameter("editButton", "Edit");
+        addRequestParameter("id", "1000");
+        doPost();
+        User userInSession = (User) getWebMockObjectFactory().getMockSession().getAttribute("user");
+        assertNotNull("Could not find user in session", user);
+        assertSame(user, userInSession);
     }
 
 }
